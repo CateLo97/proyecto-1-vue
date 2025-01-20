38ff35c1326569d92511a7c0c92d98c1d406dd71
CateLo97 <lobatocatelo@gmail.com>
2025-01-20 18:13:17 -0300

    primer commit

---
+++ proyecto-vue/src/components/InputTag.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+const tags = ref(['Tags']);
+const currentValue = ref('');
+
+
+//metodo para añadir un nuevo tag
+
+const handleKeyDown = (e)=>{
+    if(e.key==='Enter' && currentValue.value.trim() !== ''){
+        tags.value.push(currentValue.value);
+        currentValue.value= '';
+    }
+};
+
+//metodo para eliminar un tag
+
+const deleteTag = (tag) => {
+    tags.value = tags.value.filter( item => item !== tag)
+};
+
+</script>
+
+<template>
+    <div class="inputTag">
+        <div class="tags">
+            <div class="tag" v-for="(tag, index) in tags" :key="index">
+                {{ tag }} <button @click="deleteTag(tag)">❗❌</button>
+            </div>
+        </div>
+        <input type="text" v-model="currentValue" @keydown="handleKeyDown" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
